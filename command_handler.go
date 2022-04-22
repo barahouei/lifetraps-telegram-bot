c7ab59f6c7378ce7eba3393cc149511a68c78197
barahouei <abdulghanibarahouei@gmail.com>
2022-04-22 14:15:33 +0430

    Inserting user to the database in first interaction

--- command_handler.go
+++ command_handler.go
@@ -10,6 +10,8 @@ func commandHandling(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
 	if update.Message.Command() == "start" {
+		go checkUserExists(update)
+
 		msg.Text = greeting
 		msg.ReplyMarkup = mainMenuKeyboard
 	} else if update.Message.Command() == "guide" {
---
+++ users.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type users struct {
+	telegramID int64
+	username   string
+	firstname  string
+	lastname   string
+}
+
+//This function check if user already existed or not,
+//and if user can not be found in the database it add the user to database.
+func checkUserExists(update tgbotapi.Update) {
+	user := users{}
+	var userID int64
+	var existed bool
+
+	user.telegramID = update.Message.From.ID
+	user.username = update.Message.From.UserName
+	user.firstname = update.Message.From.FirstName
+	user.lastname = update.Message.From.LastName
+
+	db := dbConnect()
+	defer db.Close()
+
+	err := db.QueryRow("SELECT telegram_id FROM users WHERE telegram_id=?", user.telegramID).Scan(&userID)
+	if err != nil {
+		existed = false
+	} else {
+		existed = true
+	}
+
+	if !existed {
+		stmt, err := db.Prepare("INSERT INTO users(telegram_id, username, firstname, lastname) VALUES(?, ?, ?, ?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = stmt.Exec(user.telegramID, user.username, user.firstname, user.lastname)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
