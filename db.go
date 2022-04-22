7a0f1866a8a6d153613485c81a0cb06cee799a20
barahouei <abdulghanibarahouei@gmail.com>
2022-04-22 13:40:29 +0430

    Database can be used.

---
+++ db.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var dbname = os.Getenv("LIFETRAPS_DBNAME")
+var dbUsername = os.Getenv("DB_USERNAME")
+var dbPassword = os.Getenv("DB_PASSWORD")
+
+func dbConnect() *sql.DB {
+	db, err := sql.Open("mysql", dbUsername+":"+dbPassword+"@/"+dbname)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to database.")
+	return db
+}
