cde4f9cfd0b736d30753ca7324f605269800dc2e
barahouei <abdulghanibarahouei@gmail.com>
2023-05-16 00:44:50 +0330

    fix: safe handle

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -23,25 +23,46 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil && update.Message.IsCommand() {
-			userID := update.Message.Chat.ID
-			msgID := update.Message.MessageID
+		go SafeHandle(bot, update)
+	}
+}
 
-			for i := 1; i < msgID; i++ {
+// SafeHandle safely handles every request we have
+// and if any error happens it will recover from error and does not let bot break.
+func SafeHandle(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("*Somthing is wrong!", err)
+		}
+	}()
+
+	handleAll(bot, update)
+}
+
+// handleAll handles all request.
+func handleAll(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	if update.Message != nil && update.Message.IsCommand() {
+		userID := update.Message.Chat.ID
+		msgID := update.Message.MessageID
+
+		go func() {
+			for i := msgID - 20; i < msgID; i++ {
 				go bot.Request(tgbotapi.NewDeleteMessage(userID, i))
 			}
+		}()
 
-			go commandHandling(bot, update)
-		} else if update.CallbackQuery != nil {
-			go bot.Request(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
+		go commandHandling(bot, update)
+	} else if update.CallbackQuery != nil {
+		go bot.Request(tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID))
 
-			go callbackHandling(bot, update)
-		} else if update.Message != nil {
-			go bot.Request(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
+		go callbackHandling(bot, update)
+	} else if update.Message != nil {
+		go bot.Request(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
 
-			go messageHandling(bot, update)
-		} else {
-			continue
-		}
+		go messageHandling(bot, update)
+	} else {
+		go bot.Request(tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID))
+
+		go messageHandling(bot, update)
 	}
 }
